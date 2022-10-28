527dd916b6d76f66363fd49938baa304db7b28f9
DanielaParra <dparralp396@gmail.com>
2022-10-28 15:12:29 -0400

    Final function to update comment section UL done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.formEl = document.querySelector(".blog-form");
 app.nameInput = document.querySelector("#blog-form-name");
 app.messageInput = document.querySelector("#blog-form-textarea");
 app.emailInput = document.querySelector("#blog-form-email");
-// Format for Date object used to produce the H5s
+// Format for Date object used to produce the timestamps in the H5s as close to base style as I can get them
 app.dateFormat = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 
 // Add event listener to form on submit, stores user info into variables
@@ -19,66 +19,65 @@ app.handleSubmit = () => {
         const currentDate = new Date().toLocaleDateString("en-us", app.dateFormat);
         
         // Empties the input fields
-        app.nameInput.value = "";
-        app.messageInput.value = "";
-        app.emailInput.value = "";
+        app.emptyInputs();
         
         // Passes user inputs into a function that creates the comment div
         app.createCommentDiv(currentDate, userName, userMessage);
     });
 }
 
-// Create list item with all the divs etc inside with innerHTML
-app.createNewLi = () => {
-    const newCommentLi = document.createElement("li");
-    newCommentLi.innerHTML =
-        `<li>
-            <div class="blog-comments-img">
-                <img src="./sanctuary-helpers-final-images/comment-image2.jpg" alt="User profile photo">
-            </div>
-        </li>`
-    console.log(newCommentLi);
-}
-
-// HTML I need 
-    // will need variables for date, name of user, and message 
-
-/* <li>
-    <div class="blog-comments-img">
-        <img src="./sanctuary-helpers-final-images/comment-image2.jpg" alt="User profile photo">
-    </div>
-    <div class="blog-comments-text">
-        <h5>Wednesday October 10th, 2019 by ${name}</h5> 
-        <p>user message here</p>
-    </div>
-</li> */
-
+// Function that creates the .blog-comments-text div and inserts user inputs
 app.createCommentDiv = (date, name, message) => {
     // Creates a new div and grants it the appropriate class
-    const newDiv = document.createElement("div");
-    newDiv.className = "blog-comments-text";
-    
-    // Creates div children elements
+    const commentTextDiv = document.createElement("div");
+    commentTextDiv.className = "blog-comments-text";
+
+    // Creates elements that go in the div
     const newDateHeading = document.createElement("h5");
     const newParagraph = document.createElement("p");
 
+    // Stops comments with an empty textarea from being posted
     if (message.trim()) {
-
         // Populates elements and appends to div as children
         newDateHeading.textContent = `${date} by ${name}`;
         newParagraph.textContent = message;
-        newDiv.appendChild(newDateHeading);
-        newDiv.appendChild(newParagraph);
+        commentTextDiv.appendChild(newDateHeading);
+        commentTextDiv.appendChild(newParagraph);
 
         // Passes the populated div to a function that constructs the rest of the LI
-        app.createNewLi(newDiv);
+        app.postNewLi(commentTextDiv);
     }
 }
 
-// Append list item to ul 
+// Function that creates the rest of the LI and appends it to the UL. 
+app.postNewLi = (secondDiv) => {
+    const newCommentLi = document.createElement("li");
+    // Creates a div for the img, updates its class and innerHTML
+    const newImgDiv = document.createElement("div");
+    newImgDiv.className = "blog-comments-img";
+    // Using placekitten as there is no way to retrieve a user photo at this time
+    newImgDiv.innerHTML = `<img src="http://placekitten.com/200/200" alt="User profile photo">`
+
+    // Appends the image div, then the text div to the LI as children
+    newCommentLi.appendChild(newImgDiv);
+    newCommentLi.appendChild(secondDiv);
+    
+    // Appends the entire LI to the .blog-comments-display UL
+    app.ulEl.appendChild(newCommentLi);
+}
+
+// Empties the input fields
+app.emptyInputs = () => {
+    app.nameInput.value = "";
+    app.messageInput.value = "";
+    app.emailInput.value = "";
+}
 
 app.init = () => {
+    // Calls the event listener function on page load
     app.handleSubmit();
-};
+    // Clears the inputs in case there had been some text in them prior and the page was refreshed
+    app.emptyInputs();
+}
 
 app.init();
