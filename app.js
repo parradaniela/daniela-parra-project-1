143300028f758af480c4e70cf1db3c03c1b41073
DanielaParra <dparralp396@gmail.com>
2022-10-29 10:46:59 -0400

    Last tweaks for better comments, more consistent variable names

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const app = {};
 
+// *** This program applies only to the form and comment section in the blog.html page *** //
+
 // Form and UL elements stored into variables
 app.ulEl = document.querySelector(".blog-comments-display");
 app.formEl = document.querySelector(".blog-form");
 app.nameInput = document.querySelector("#blog-form-name");
 app.messageInput = document.querySelector("#blog-form-textarea");
 app.emailInput = document.querySelector("#blog-form-email");
-// Format for Date object used to produce the timestamps in the H5s as close to base style as I can get them
+// Format for Date object created in the form event listener function
 app.dateFormat = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 
 // Add event listener to form on submit, stores user info into variables
 app.handleSubmit = () => {
     app.formEl.addEventListener("submit", (event) => {
         event.preventDefault();
-        // Stores user inputs
+        // Stores user inputs and captures date the event was triggered
         const userName = app.nameInput.value;
         const userMessage = app.messageInput.value;
         const currentDate = new Date().toLocaleDateString("en-us", app.dateFormat);
@@ -22,12 +24,12 @@ app.handleSubmit = () => {
         app.emptyInputs();
         
         // Passes user inputs into a function that creates the comment div
-        app.createCommentDiv(currentDate, userName, userMessage);
+        app.populateCommentDiv(currentDate, userName, userMessage);
     });
 }
 
-// Function that creates the .blog-comments-text div and inserts user inputs
-app.createCommentDiv = (date, name, message) => {
+// Function that creates the .blog-comments-text div and populates it with the user inputs
+app.populateCommentDiv = (date, name, message) => {
     // Creates a new div and grants it the appropriate class
     const commentTextDiv = document.createElement("div");
     commentTextDiv.className = "blog-comments-text";
@@ -52,14 +54,13 @@ app.createCommentDiv = (date, name, message) => {
 // Function that creates the rest of the LI and appends it to the UL. 
 app.postNewLi = (textDiv) => {
     const newCommentLi = document.createElement("li");
-    // Creates a div for the img, updates its class and innerHTML
-    const newImgDiv = document.createElement("div");
-    newImgDiv.className = "blog-comments-img";
-    // Using placekitten as there is no way to retrieve a user photo at this time
-    newImgDiv.innerHTML = `<img src="http://placekitten.com/94/92" alt="User profile photo">`
+    // Creates a div for the img, updates its class and innerHTML using placekitten as there is no way to retrieve a user photo at this time
+    const commentImgDiv = document.createElement("div");
+    commentImgDiv.className = "blog-comments-img";
+    commentImgDiv.innerHTML = `<img src="http://placekitten.com/94/92" alt="User profile photo">`
 
     // Appends the image div, then the passed text div to the LI as children
-    newCommentLi.appendChild(newImgDiv);
+    newCommentLi.appendChild(commentImgDiv);
     newCommentLi.appendChild(textDiv);
     
     // Appends the entire LI to the .blog-comments-display UL
